ebf9da80ae5d33218d1c5e4cf0e9a716d5b25ae4
Екатерина Песня <ekaterinapesna@MacBook-Air-Ekaterina.local>
2020-12-25 20:18:37 +0300

    fix error

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,26 @@
 
-let like = document.querySelector('.card__like');
-like.addEventListener('click', () => {
-  like.classList.toggle('card__like_active')
-});
-
+let likes = document.querySelectorAll('.card__like');
 let openButton = document.querySelector('.profile__popup-button');
 let overlay = document.querySelector('.overlay');
 let closeButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__container')
+let nameInput = document.querySelector('[name="popup__name"]')
+let jobInput = document.querySelector('[name="popup__job"]')
+let titleName = document.querySelector('.profile__title')
+let descriptionName = document.querySelector('.profile__description')
+
+likes.forEach((like) => {
+  like.addEventListener('click', () => {
+    like.classList.toggle('card__like_active')
+  });
+});
 
 let togglePopup = () => {
   overlay.classList.toggle('overlay_opened')
+  nameInput.value = titleName.textContent
+  jobInput.value = descriptionName.textContent
 }
+
 openButton.addEventListener('click', togglePopup)
 closeButton.addEventListener('click', togglePopup)
 overlay.addEventListener('click', (event) => {
@@ -18,21 +28,13 @@ overlay.addEventListener('click', (event) => {
     togglePopup()
   }
 })
-let formElement = document.querySelector('.popup__container')
-let nameInput = document.querySelector('.popup__name')
-let jobInput = document.querySelector('.popup__job')
-let titleName = document.querySelector('.profile__title')
-let descriptionName = document.querySelector('.profile__description')
-nameInput.value = titleName.textContent
-jobInput.value = descriptionName.textContent
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-
   titleName.textContent = nameInput.value
   descriptionName.textContent = jobInput.value
-
   togglePopup()
 }
+
 
 formElement.addEventListener('submit', handleFormSubmit);
